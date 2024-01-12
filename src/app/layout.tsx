5e3edc86cfc4aa06da3f135bc585d0465f8ae239
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-01-12 17:51:43 +0000

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 import "./globals.css"
 
@@ -69,6 +70,7 @@ export default function RootLayout(params: { children: React.ReactNode }) {
               </div>
             </footer>
           </div>
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
